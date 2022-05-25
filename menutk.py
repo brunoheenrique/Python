3e5d32c2a5a005607a70e872d3c26b8881d3d98d
brunoheenrique <bruno180396@gmail.com>
2022-05-25 12:29:30 -0300

    novos testes menu

--- menutk.py
+++ menutk.py
@@ -1,39 +1,41 @@
 from tkinter import *
 
 def donothing():
-   filewin = Toplevel(root)
-   button = Button(filewin, text="Do nothing button")
-   button.pack()
+   filewin = Toplevel(root);
+   button = Button(filewin, text="Do nothing button");
+   button.pack();
    
-root = Tk()
-menubar = Menu(root)
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="New", command=donothing)
-filemenu.add_command(label="Open", command=donothing)
-filemenu.add_command(label="Save", command=donothing)
-filemenu.add_command(label="Save as...", command=donothing)
-filemenu.add_command(label="Close", command=donothing)
+root = Tk();
 
-filemenu.add_separator()
+menubar = Menu(root);
+filemenu = Menu(menubar, tearoff=0);
+filemenu.add_command(label="New", command=donothing);
+filemenu.add_command(label="Open", command=donothing);
+filemenu.add_command(label="Save", command=donothing);
+filemenu.add_command(label="Save as...", command=donothing);
+filemenu.add_command(label="Close", command=donothing);
 
-filemenu.add_command(label="Exit", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu)
-editmenu = Menu(menubar, tearoff=0)
-editmenu.add_command(label="Undo", command=donothing)
+filemenu.add_separator();
 
-editmenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit);
+menubar.add_cascade(label="File", menu=filemenu);
+editmenu = Menu(menubar, tearoff=0);
+editmenu.add_command(label="Undo", command=donothing);
 
-editmenu.add_command(label="Cut", command=donothing)
-editmenu.add_command(label="Copy", command=donothing)
-editmenu.add_command(label="Paste", command=donothing)
-editmenu.add_command(label="Delete", command=donothing)
-editmenu.add_command(label="Select All", command=donothing)
+editmenu.add_separator();
 
-menubar.add_cascade(label="Edit", menu=editmenu)
-helpmenu = Menu(menubar, tearoff=0)
-helpmenu.add_command(label="Help Index", command=donothing)
-helpmenu.add_command(label="About...", command=donothing)
-menubar.add_cascade(label="Help", menu=helpmenu)
+editmenu.add_command(label="Cut", command=donothing);
+editmenu.add_command(label="Copy", command=donothing);
+editmenu.add_command(label="Paste", command=donothing);
+editmenu.add_command(label="Delete", command=donothing);
+editmenu.add_command(label="Select All", command=donothing);
 
-root.config(menu=menubar)
-root.mainloop()
+menubar.add_cascade(label="Edit", menu=editmenu);
+helpmenu = Menu(menubar, tearoff=0);
+helpmenu.add_command(label="Help Index", command=donothing);
+helpmenu.add_command(label="About...", command=donothing);
+menubar.add_cascade(label="Help", menu=helpmenu);
+
+root.geometry("320x320+0+0");
+root.config(menu=menubar);
+root.mainloop();
