4fadda271465ef79b676ef616323349fb98991b2
brunoheenrique <bruno180396@gmail.com>
2022-05-25 12:47:43 -0300

    organizando os codigos

--- menutk.py
+++ menutk.py
@@ -9,6 +9,8 @@ root = Tk();
 menubar = Menu(root);
 
 filemenu = Menu(menubar, tearoff=0);
+menubar.add_cascade(label="File", menu=filemenu);
+
 filemenu.add_command(label="New", command=donothing);
 filemenu.add_command(label="Open", command=donothing);
 filemenu.add_command(label="Save", command=donothing);
@@ -17,9 +19,9 @@ filemenu.add_command(label="Close", command=donothing);
 filemenu.add_separator();
 filemenu.add_command(label="Exit", command=root.quit);
 
-menubar.add_cascade(label="File", menu=filemenu);
-
 editmenu = Menu(menubar, tearoff=0);
+menubar.add_cascade(label="Edit", menu=editmenu);
+
 editmenu.add_command(label="Undo", command=donothing);
 editmenu.add_separator();
 editmenu.add_command(label="Cut", command=donothing);
@@ -28,13 +30,12 @@ editmenu.add_command(label="Paste", command=donothing);
 editmenu.add_command(label="Delete", command=donothing);
 editmenu.add_command(label="Select All", command=donothing);
 
-menubar.add_cascade(label="Edit", menu=editmenu);
-
 helpmenu = Menu(menubar, tearoff=0);
-helpmenu.add_command(label="Help Index", command=donothing);
-helpmenu.add_command(label="About...", command=donothing);
 menubar.add_cascade(label="Help", menu=helpmenu);
 
-root.geometry("320x320+0+0");
+helpmenu.add_command(label="Help Index", command=donothing);
+helpmenu.add_command(label="About...", command=donothing);
+
+root.geometry("220x220+10+10");
 root.config(menu=menubar);
 root.mainloop();
