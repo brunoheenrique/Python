77bc6dee41b086bb6de87fcc2b0d6243836a7d7d
brunoheenrique <bruno180396@gmail.com>
2022-05-25 13:02:18 -0300

    novos testes menu

--- menutk.py
+++ menutk.py
@@ -36,6 +36,13 @@ menubar.add_cascade(label="Help", menu=helpmenu);
 helpmenu.add_command(label="Help Index", command=donothing);
 helpmenu.add_command(label="About...", command=donothing);
 
-root.geometry("220x220+10+10");
+titulo = Label(root,text="Testes Form de Python",fg='red',font=('Arial',18));
+titulo.place(x=60,y=0);
+
+botao= Button(root,text="Inserir",fg="red",bd=5,width= 7, height= 1);
+botao.place(x=12,y=180);
+
+root.title("Bem Vindo Menu TkInter")
+root.geometry("300x220+10+10");
 root.config(menu=menubar);
 root.mainloop();
