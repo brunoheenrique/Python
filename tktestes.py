812e944b4f8481636921fbaf994a225dcce1296a
brunoheenrique <bruno180396@gmail.com>
2022-05-25 12:17:21 -0300

    final primeiro teste

--- tktestes.py
+++ tktestes.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 import tkinter as tk
 
 window = tk.Tk();
@@ -7,9 +8,6 @@ lbl = tk.Label(window,text= "Primeiro Rótulo de teste", fg= 'red',font=('Calibr
 lbl.place(x=60,y=0);
 txtfld = tk.Entry(window, text= "Primeiro TextBox de teste", bd= 4);
 txtfld.place(x=80,y=150);
-mnu = tk.Menu(window,activebackground='blue',title="Menuzão");
-mnubtn = tk.Menubutton(window,text="Botão Menu",fg='black');
-mnubtn.place(x=80,y=200);
 window.title("Bem vindo à Janela de Testes");
 window.geometry("320x320+10+10");
 window.mainloop();
