ba596be63af55fa019b1ec49c4f4ca969d326743
brunoheenrique <bruno180396@gmail.com>
2022-05-25 11:45:26 -0300

    novos testes python

--- tktestes.py
+++ tktestes.py
@@ -1,12 +1,15 @@
 import tkinter as tk
 
-window = tk.TK();
+window = tk.Tk();
 btn = tk.Button(window,text="Botão Teste1", fg='blue');
 btn.place(x=80,y=100);
-lbl = tk.Label(window,text= "Primeiro Rótulo de teste", fg= 'red',font=('Calibri',18));
-lbl.place(x=60,y=50);
-txtfld = tk.Entry(window, text= "Primeiro TextBox de teste", bd= 5);
+lbl = tk.Label(window,text= "Primeiro Rótulo de teste", fg= 'red',font=('Calibri',16));
+lbl.place(x=60,y=0);
+txtfld = tk.Entry(window, text= "Primeiro TextBox de teste", bd= 4);
 txtfld.place(x=80,y=150);
+mnu = tk.Menu(window,activebackground='blue',title="Menuzão");
+mnubtn = tk.Menubutton(window,text="Botão Menu",fg='black');
+mnubtn.place(x=80,y=200);
 window.title("Bem vindo à Janela de Testes");
-window.geomety("300x200+10+10");
+window.geometry("320x320+10+10");
 window.mainloop();
