dd2b38ce92ed9a99faa63cf950af419b83273d3b
brunoheenrique <bruno180396@gmail.com>
2022-05-26 09:15:21 -0300

    testes em OpenFile

--- openFile.py
+++ openFile.py
@@ -2,6 +2,14 @@ arquivo  = open("teste.txt");
 
 arquivo2 = open("C:/Dev/WebDesign/Webdesign/teste2.txt")
 
-conteudo = arquivo.readlines();
+conteudo = arquivo.readline();
+conteudo2 = arquivo.readline();
+conteudo3 = arquivo.readline();
 
 print(repr(conteudo));
+print(repr(conteudo2));
+print(repr(conteudo3));
+
+arquivo.close()
+
+arquivo2.close()
