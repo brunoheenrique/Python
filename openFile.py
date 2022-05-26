af04cfe6b496842e1211c88f0a6b06fdfdbee091
brunoheenrique <bruno180396@gmail.com>
2022-05-26 09:21:47 -0300

    testando laços for com leitura

--- openFile.py
+++ openFile.py
@@ -1,12 +1,7 @@
 arquivo  = open("teste.txt", "r");
 
-conteudo = arquivo.readline();
-conteudo2 = arquivo.readline();
-conteudo3 = arquivo.readline();
-
-print(repr(conteudo));
-print(repr(conteudo2));
-print(repr(conteudo3));
+for linha in arquivo:
+    print(repr(linha));
 
 arquivo.close()
 
