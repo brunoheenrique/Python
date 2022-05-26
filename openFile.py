34a414d8db77e3a08ee7288b13b5540f8203f8ef
brunoheenrique <bruno180396@gmail.com>
2022-05-26 09:26:03 -0300

    testes file

--- openFile.py
+++ openFile.py
@@ -2,6 +2,11 @@ arquivo  = open("teste.txt", "r");
 
 for linha in arquivo:
     print(repr(linha));
+    
+arquivo.seek(0);
+conteudo = arquivo.read();
+
+print(repr(conteudo));
 
 arquivo.close()
 
