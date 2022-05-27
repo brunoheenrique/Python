ece4ac9cf4f4e6b61538ab62198308a390775d5d
brunoheenrique <bruno180396@gmail.com>
2022-05-27 16:20:41 -0300

    testes no banco de dados

--- cadastro/conectDB.py
+++ cadastro/conectDB.py
@@ -4,16 +4,22 @@ try:
     conexao = conector.connect("banco_teste.db")
     cursor = conexao.cursor()
     
-    comando = '''CREATE TABLE Veiculo(
-      placa CHARACTER(7) NOT NULL, ano INTEGER NOT NULL,
-      cor TEXT NOT NULL, proprietario INTEGER NOT NULL,
-      marca INTEGER NOT NULL,
-      PRIMARY KEY(placa)
-      FOREIGN KEY(proprietario) REFERENCES Pessoa(cpf),
-      FOREIGN KEY(marca) REFERENCES Marca(id)
-    );'''
+    comando1 = ''' DROP TABLE Veiculo;'''
   
-    cursor.execute(comando)
+    cursor.execute(comando1)
+    
+    comando2 = '''CREATE TABLE Veiculo(
+        placa CHARACTER(7) NOT NULL,
+        ano INTEGER NOT NULL, cor TEXT NOT NULL,
+        motor REAL NOT NULL,
+        proprietario INTEGER NOT NULL,
+        marca INTEGER NOT NULL,
+        PRIMARY KEY(placa),
+        FOREIGN KEY(proprietario) REFERENCES Pessoa(cpf),
+        FOREIGN KEY(marca) REFERENCES Marca(id)
+    );'''
+    
+    cursor.execute(comando2)
     
     conexao.commit()
     
