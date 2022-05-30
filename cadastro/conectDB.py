4014f41be3e6e25dc684be30d4f875dac83e7f2b
brunoheenrique <bruno180396@gmail.com>
2022-05-30 08:15:28 -0300

    criando classe e funçoes de conexao DB

--- cadastro/conectDB.py
+++ cadastro/conectDB.py
@@ -1,34 +1,34 @@
 import sqlite3 as conector
+class conectDB :
 
-try:
-    conexao = conector.connect("banco_teste.db")
-    cursor = conexao.cursor()
-    
-    comando1 = ''' DROP TABLE Veiculo;'''
-  
-    cursor.execute(comando1)
-    
-    comando2 = '''CREATE TABLE Veiculo(
-        placa CHARACTER(7) NOT NULL,
-        ano INTEGER NOT NULL, cor TEXT NOT NULL,
-        motor REAL NOT NULL,
-        proprietario INTEGER NOT NULL,
-        marca INTEGER NOT NULL,
-        PRIMARY KEY(placa),
-        FOREIGN KEY(proprietario) REFERENCES Pessoa(cpf),
-        FOREIGN KEY(marca) REFERENCES Marca(id)
-    );'''
-    
-    cursor.execute(comando2)
-    
-    conexao.commit()
-    
-except conector.DatabaseError as error:
-      
-    print('Erro de Banco de Dados', error)
-      
-    
-finally:
-    cursor.close()
-      
-    conexao.close()
+    def conexDB():
+        try:
+            conexao = conector.connect("banco_teste.db")
+            cursor = conexao.cursor()
+            
+        # comando1 = ''' DROP TABLE Veiculo;'''
+        
+        # cursor.execute(comando1)
+            
+        #  comando2 = '''CREATE TABLE Veiculo(
+        #        placa CHARACTER(7) NOT NULL,
+        #        ano INTEGER NOT NULL, cor TEXT NOT NULL,
+        #       motor REAL NOT NULL,
+        #       proprietario INTEGER NOT NULL,
+        #      marca INTEGER NOT NULL,
+        #     PRIMARY KEY(placa),
+            #    FOREIGN KEY(proprietario) REFERENCES Pessoa(cpf),
+            #   FOREIGN KEY(marca) REFERENCES Marca(id)
+        #  );'''
+            
+        #  cursor.execute(comando2)
+            
+        # conexao.commit()
+            
+        except conector.DatabaseError as error:
+            
+            print('Erro de Banco de Dados', error)
+        
+        finally:
+            cursor.close() 
+            conexao.close()
